64c608d1f49a8ee61e5950e7a4a5e8efb122aa3b
Luke Loecher <ljloecher@gmail.com>
2021-12-14 19:43:39 -0500

    finished part 1

---
+++ src/components/MemoryCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./MemoryCard.css";
+
+const MemoryCard = () => {
+  return <div className="MemoryCard">
+      <img src ="https://www.digitalcrafts.com/img/logo-wrench-white.png"></img>
+  </div>;
+};
+
+export default MemoryCard;
